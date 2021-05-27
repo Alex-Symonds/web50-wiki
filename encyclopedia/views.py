51267fa961a6ff42cbab79c732fef57be96a7cd7
Alex Symonds <alex-symonds@gmail.com>
2021-05-27 11:56:08 +0100

    Criteria 3 working (search)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -21,5 +21,27 @@ def entry(request, title):
             "entry_contents": entry
         })
 
+def search(request):
+    search_term = request.GET.get("q", "")
+
+    entry = util.get_entry(search_term)
+    if entry == None:
+        return render(request, "encyclopedia/results.html", {
+            "search_term": search_term,
+            "entries": [e for e in util.list_entries() if search_term.lower() in util.get_entry(e).lower()]
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": search_term,
+            "entry_contents": entry
+        })      
+
+
+    
+
+
+
+
+
 
 

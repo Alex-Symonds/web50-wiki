6892418241c4a3372bd5e8ede3099f4548d20fb6
Alex Symonds <alex-symonds@gmail.com>
2021-05-27 13:36:52 +0100

    Criteria 4 working (new page)

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("create_new", views.create_new, name="create"),
     path("wiki/<str:title>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 
 from . import util
 
@@ -11,31 +14,48 @@ def index(request):
 def entry(request, title):
     entry = util.get_entry(title)
 
-    if entry == None:
-        return render(request, "encyclopedia/error.html",{
-            "title": title
-        })
-    else:
+    if entry:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "entry_contents": entry
         })
 
+    return render(request, "encyclopedia/error.html",{
+        "title": title
+    })
+
 def search(request):
     search_term = request.GET.get("q", "")
-
     entry = util.get_entry(search_term)
-    if entry == None:
-        return render(request, "encyclopedia/results.html", {
-            "search_term": search_term,
-            "entries": [e for e in util.list_entries() if search_term.lower() in util.get_entry(e).lower()]
-        })
-    else:
+
+    if entry:
         return render(request, "encyclopedia/entry.html", {
             "title": search_term,
             "entry_contents": entry
-        })      
+        })
 
+    return render(request, "encyclopedia/results.html", {
+        "search_term": search_term,
+        "entries": [e for e in util.list_entries() if search_term.lower() in util.get_entry(e).lower()]
+    })    
+
+def create_new(request):
+
+    if request.method == "POST":
+        new_input = request.POST
+
+        # Check if the article already exists
+        if new_input["new_title"].lower() in (t.lower() for t in util.list_entries()):
+            return render(request, "encyclopedia/exists.html", {
+                "title": new_input["new_title"]
+            })
+        
+        # Save it, then redirect the user to it
+        util.save_entry(new_input["new_title"], new_input["new_content"])
+        url = reverse("entry", kwargs={'title': new_input["new_title"]})
+        return HttpResponseRedirect(url)
+ 
+    return render(request, "encyclopedia/create.html")
 
     
 
